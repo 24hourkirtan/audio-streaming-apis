ba1f5dd1072b6f99ca66eae46c56d87770f56f20
wkande <warren@wyomingsoftware.com>
2016-03-10 06:15:51 -0700

    added next and previous metadata

--- rest_v1_0_0/mp3s.js
+++ rest_v1_0_0/mp3s.js
@@ -26,28 +26,56 @@ module.exports = {
             if(aid != null){
                 var col = db.conn.collection('mp3s');
 
-                var cnt = yield col.count();
+                // Parameters
                 var limit = (req.params.limit) ? parseInt(req.params.limit) : 10;
                 var skip = (req.params.skip) ? parseInt(req.params.skip) : 0;
                 var remainingCnt = (cnt-skip-limit < 0) ? 0 : cnt-skip-limit;
-                var sort = (req.params.limit) ? parseInt(req.params.limit) : 10;
                 var sort = (req.params.sort) ? req.params.sort : 'title';
                 var order = (req.params.order == 'asc') ? -1 : 1;
+                var image = (req.params.image) ? req.params.image : true;
+                var query = (req.params.q) ? {$text:{$search:req.params.q}} : {};
+
+                // Projection
                 var projection = {"trash":0};
                 if (req.params.image == 'false'){
                     projection = {"trash":0,"image.data":0};
                 }
+
+                // Get record count from possible queries
+                var cnt = 0;
+                if(query.$text)
+                    cnt = yield col.count(query);
+                else
+                    cnt = yield col.count();
+
+                // Next and Prev
+                var q = '';
+                if(query.$text)
+                    q = req.params.q;
+                var next = null;
+                if(skip+limit < cnt)
+                    next = '/mp3s?q='+q+'&limit='+limit+'&skip='+(skip+limit)+'&sort='+sort+'&order='+order+'&image='+image;
+                var prev = null;
+                if(skip >= limit)
+                    prev = '/mp3s?q='+q+'&limit='+limit+'&skip='+(skip-limit)+'&sort='+sort+'&order='+order+'&image='+image;
+
+                // Sort
                 var sortSyntax = {};
                 sortSyntax[sort] = order;
-                var query = (req.params.q) ? {$text:{$search:req.params.q}} : {};
+
+                // Data
                 var docs = yield col.find(query,projection).limit(limit).skip(skip).sort(sortSyntax).toArray();
+                var remainingCnt = ((cnt-skip-limit) < 0) ? 0 : (cnt-skip-limit);
                 res.send(200, { _limit:limit,
+                                _q:q,
                                 _skip:skip,
-                                _collectionCnt:cnt,
+                                _totalCnt:cnt,
                                 _remainingCnt:remainingCnt,
                                 _returnedCnt:docs.length,
                                 _sort:sort,
                                 _order:order,
+                                _next:next,
+                                _prev:prev,
                                 mp3s:docs});
                 return next();
             }
