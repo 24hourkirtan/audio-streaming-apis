a4c4b0fa438089b1c90707b3b6a9c21f40a2b36b
wkande <warren@wyomingsoftware.com>
2016-03-14 09:19:49 -0600

    added code comment blocks

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,14 +1,21 @@
 var jsonwebtoken = require('jsonwebtoken');
 var config = require("../config.json") [process.env.NODE_ENV];
-
-
-console.log("|>>>  jwt.js loaded");
-
 var SECRET = config.jwt_secret;
 var APPKEY = config.jwt_appkey; // Try not to change the app key or everyone gets logged out
 
+/**
+ * Export all functions that manage JWT tokns
+ * @type {Object}
+ */
 module.exports = {
 
+    /**
+     * Creates and returns a JWT token using the account _id, logintype (local or fb)
+     * using the APPKEY and SECRET from the config.json file.
+     * @param  {string} aid        the _id from the accounts collection
+     * @param  {string} loginType  the type of login the account uses local or fb)
+     * @return {json}              a json formatted JWT token
+     */
  	  getToken: function (aid, loginType) {
             try{
                   return jsonwebtoken.sign({aid:aid, type:loginType, appKey:APPKEY}, SECRET);
@@ -17,28 +24,39 @@ module.exports = {
                 throw new Error("Failed to generate jwt token: "+err.toString());
             }
 	  },
+
+    /**
+     * Gets a sinlge mp3 record using the record id
+     * @param  {object}   req   request
+     * @param  {object}   res   respone
+     * @param  {next}     next  restify route pattern
+     * @return {next}           restify route pattern
+     */
+
+    /**
+     * Extractes the JWT token from the request header and verifies it is valid. This
+     * function is not exposed to any API endpoint and is only used by endpoints
+     * to valid the user.
+     * @param  {object}   req   request
+     * @param  {object}   res   respone
+     * @param  {next}     next  restify route pattern
+     * @return {string}         the _id of the account or null if token is invalid
+     */
     verifyToken: function(req, res, next){
           try{
               var token = req.headers.jwt;
-              //console.log('verifyToken', token);
               var decoded = jsonwebtoken.verify(token, SECRET);
-              console.log("\n|----> verifyToken: "+JSON.stringify(decoded));
-              if(decoded.appKey != APPKEY){
-                  console.log("appKey wrong or missing");
+              if(decoded.appKey != APPKEY){ // appKey wrong or missing
                   res.send(401, 'NOT AUTH: token verification');
                   return null;
               }
               return decoded.aid;
           }
           catch(err){
-              console.log("|----> JWT Error, verifyToken");
               console.log(err);
               res.send(401, err);
               return null;
           }
-    },
-    getNotAuthMsg(){
-        return 'not authoized';
     }
 
 };
