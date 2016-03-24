1551eaa656f8ff1686323e8100a0c30ae33017a7
wkande <warren@wyomingsoftware.com>
2016-03-24 08:00:46 -0600

    new jingles endpoint file, w/GET /jingle/random

---
+++ rest_v1_0_0/jingles.js
@@ -0,0 +1,46 @@
+var db = require('../ops/db'),
+    co = require('co');
+const assert = require('assert');
+var jwt = require("../utils/jwt.js");
+var ObjectID = require('mongodb').ObjectID;
+var db = require('../ops/db');
+
+/**
+ * Export all functions that manage the database JINGLES collection
+ * @type {Object}
+ */
+module.exports = {
+
+    /**
+     * Gets a single random jingle record using the record id
+     * @param  {object}   req   request
+     * @param  {object}   res   respone
+     * @param  {next}     next  restify route pattern
+     * @return {next}           restify route pattern
+     */
+    getRandom: function(req, res, next) {
+        res.setHeader('X-Version', '1.0.0');
+        var aid = jwt.verifyToken(req, res, next);
+        var projection = {};
+        if (req.params.image == 'false'){
+            projection = {"image.data":0};
+        }
+        co(function*() {
+            if(aid != null){
+
+                var col = db.conn.collection('jingles');
+                var cnt = yield col.count();
+                var skip = Math.floor((Math.random() * cnt));
+                var docs = yield col.find( {}, projection ).limit(1).skip(skip).toArray();
+                assert.ok((docs.length < 2), 'single record find returned more than one record');
+                assert.ok((docs.length == 1), 'a random jingle was not found');
+                res.send(200, docs[0]);
+                return next();
+            }
+        }).catch(function(err) {
+            db.insertLogs('ERROR: (jingle.getRandom) '+err);
+            res.send(500, err);
+            return next();
+        });
+    }
+};
