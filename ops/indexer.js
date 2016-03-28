42d4540457b05d620f7d85533ab43f8f8d31e100
wkande <warren@wyomingsoftware.com>
2016-03-28 13:03:27 -0600

    added logo encoded64 string for id3 tags missing picture

--- ops/indexer.js
+++ ops/indexer.js
@@ -1,12 +1,13 @@
 var path = require('path');
 var recursive = require('recursive-readdir');
 var fes = require('forEachAsync');
+var fs = require('fs');
 var jsmediatags = require("jsmediatags");
 var ObjectID = require('mongodb').ObjectID;
-var fs = require("fs");
 var db = require('../ops/db'),
     co = require('co'),
     assert = require('assert');
+
 
 /**
  * Export all functions that manage the Indexer process
@@ -27,72 +28,145 @@ module.exports = {
      * Promises with YIELD are used to throttle the UPSERTS which could overwhelm a
      * virtual CPU.
      *
-     * @param  {string} directoryPath path to recursively process files
+     * @param  {string} directoryPath   path to recursively process files
+     * @param  {string} collecction     the db collection for upsert operation
      * @return {nothing}
      */
-    run: function(directoryPath) {
+    run: function(directoryPath, collection) {
         console.log('\n>>> ---- Indexer run ----------------------------',
+          '\n>>> '+collection,
           '\n>>> '+directoryPath,
-          '\n------------------------------------------------');
+          '\n-------------------------------------------------');
 
+        getLogoImage();
+
+
+
+        // /Users/warren/Downloads/_media/jingles/160130-133452-edit.mp3
+        // /Users/warren/Downloads/_media/jingles/160130-135615-edit.mp3
+        /*console.log('READY')
+        new jsmediatags.Reader('/Users/warren/Downloads/_media/jingles/160130-135615-edit.mp3')
+        .setTagsToRead(["title", "artist", "year", "album", "year", "genre", "picture", "size"])
+        .read({
+            onSuccess: function(id3) {
+              console.log(id3)
+            },
+            onError: function(error) {
+                console.log(error);
+            }
+        });
+
+
+        return;*/
         recursive(directoryPath, function (err, files) {
+
             // Files is an array of filename
-            if(typeof files == 'undefined' || files == null){
-                db.insertLogs('ERROR: (Indexer) Start path invalid: '+directoryPath);
+            if(err){
+                db.insertLogs('ERROR: (Indexer) '+err);
                 return;
             }
+            else if(typeof files == 'undefined' || files == null){
+                db.insertLogs('ERROR: (Indexer) Path invalid: '+directoryPath);
+                return;
+            }
+
             fes.forEachAsync(files, function (next, file, index, array) {
                 if(path.extname(file) == ".mp3"){
-                    jsmediatags.read(file, {
-                        onSuccess: function(id3) {
-                            upsertMP3(file, id3, next);
-                        },
-                        onError: function(error) {
-                            db.insertLogs('ERROR: (Indexer) jsmediatags.read: '+ error);
-                            next();
-                        }
-                    });
+                    try{
+                        new jsmediatags.Reader(file).setTagsToRead(["title", "year", "album", "year", "genre", "picture", "size"])
+                        .read({
+                            onSuccess: function(id3) {
+                              upsertMP3(file, id3, collection, next);
+                            },
+                            onError: function(error) {
+                                db.insertLogs('ERROR: (Indexer) jsmediatags.read: '+ error);
+                                next();
+                            }
+                        });
+                    }
+                    catch(err){
+                        db.insertLogs('ERROR: (Indexer) jsmediatags.read.outer.try-block: '+ err);
+                        next();
+                    }
                 }
                 else{
                     next();
                 }
             }).then(function () {
-                db.insertLogs('Indexer: All files finished for :'+directoryPath);
+                ;
             });
         });
     },
 
     /**
-     * Steps through the mp3s database collection and checks the path of the record
+     * Steps through the mp3s or jingles database collections and checks the path of the record
      * still exists in the file system. If it does not the record's orphaned key is set to
-     * true. Orphaned records are not included in GET /mp3s but are in GET /mp3:_id.
+     * true.
+     *
+     * Orphaned records are not included in:
+     * GET /mp3s
+     * GET /jingles
+     *
+     * Orphaned records are included in:
+     * GET /mp3:_id
+     * GET /jingles:_id
      * @return {none}
      */
-    tagOrphans(){
-        console.log('\n>>> ---- Indexer tagOrphans ----------------------------');
-        var col = db.conn.collection('mp3s');
-        col.find().forEach(function(doc){
-            fs.exists(doc.path,function(exists){
-                if (!exists){
-                    col.findOneAndUpdate({_id:ObjectID(doc._id)},
-                        {$set: {orphaned:true}},
-                        {returnOriginal: false, upsert: false}
-                    );
-                } // end if
-            }); // end fs.exists
-        }); // end col.find()
+    tagOrphans(collection){
+      try{
+          console.log('\n>>> ---- Indexer tagOrphans --------------------',
+            '\n>>> '+collection,
+            '\n------------------------------------------------');
+          var col = db.conn.collection(collection);
+          col.find().forEach(function(doc){
+              fs.exists(doc.path,function(exists){
+                  if (!exists){
+                      col.findOneAndUpdate({_id:ObjectID(doc._id)},
+                          {$set: {orphaned:true}},
+                          {returnOriginal: false, upsert: false}
+                      );
+                  } // end if
+              }); // end fs.exists
+          }); // end col.find()
+      }
+      catch(err){
+          db.insertLogs('ERROR: (Indexer) tagOrphans.try-block: '+ err);
+      }
     }
 };
 
+var logoImage = null;
+function getLogoImage(){
+    if(logoImage == null){
+        fs.readFile('default-image', (err, data) => {
+            if (err) {
+                db.insertLogs('ERROR: (Indexer) getLogoImage: '+ err);
+                return err.toString();
+            }
+            logoImage = data;
+            return logoImage;
+        });
+    }
+    else{
+        return logoImage;
+    }
+}
+
 /**
  * [upsertMP3 description]
- * @param  {string} file    the path to the MP3 file
- * @param  {Object} id3     the extracted ID3 tag metadata
+ * @param  {string} file            the path to the MP3 file
+ * @param  {Object} id3             the extracted ID3 tag metadata
+ * @param  {string} collecction     the db collection for upsert operation
+ * @param  {function} callback      the callback for fes.forEachAsync operation
  * @return {Promise}
  */
-function upsertMP3(file, id3, callback){
-    //console.log("--------------------------",id3)
-    var collection = db.conn.collection('mp3s');
+function upsertMP3(file, id3, collection, callback){
+    var collection = db.conn.collection(collection);
+    if(!id3.tags.picture){
+        id3.tags.picture = {};
+        id3.tags.picture.format = 'image/png';
+        id3.tags.picture.data = logoImage;
+    }
     co(function* () {
         var result = yield collection.findOneAndUpdate({path:file},
               {$set: {title: id3.tags.title,
@@ -114,6 +188,7 @@ function upsertMP3(file, id3, callback){
         //console.log(data);
         callback();
     }, function (err) {
-        db.insertLogs('ERROR: (Indexer) upsertMP3.co: '+ err);
+        db.insertLogs('ERROR: (Indexer) upsertMP3.co(): '+file+': '+ err, id3);
+        callback();
     });
 }
